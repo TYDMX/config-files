523a891310fa2a83c305143a8537f84068b61897
TYDMX <69235273+TYDMX@users.noreply.github.com>
2026-04-01 19:20:02 +0800

    更新 融合订阅.js

--- 脚本/融合订阅.js
+++ 脚本/融合订阅.js
@@ -257,7 +257,8 @@ function main(config) {
         "GEOSITE,category-games-cn,🎮 game@CN",
         "OR,((GEOSITE,google@cn),(GEOSITE,googlefcm)),🇬 谷歌@CN",
         "GEOSITE,category-games-!cn,🎮 game",
-        "AND,((DST-PORT,443),(NETWORK,UDP),(NOT,((GEOSITE,cn),(GEOIP,CN)),REJECT",
+
+        "AND,((NETWORK,UDP),(DST-PORT,443),NOT,(OR,((GEOSITE,CN),(GEOIP,CN)))),REJECT",
 
         "OR,((GEOSITE,openai),(GEOSITE,google-gemini),(GEOSITE,category-ai-!cn)),🤖 人工智能",
         "GEOSITE,spotify,🇯🇵 日本故转",
