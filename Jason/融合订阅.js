84e51657f8351f02b31d6925aa59b3628e167565
TYDMX <69235273+TYDMX@users.noreply.github.com>
2026-04-08 20:56:53 +0800

    Add a new rule set to fake IP filter

--- Jason/融合订阅.js
+++ Jason/融合订阅.js
@@ -148,7 +148,7 @@ function main(config) {
         "fake-ip-filter-mode": "rule",
         "fake-ip-filter": [
             "GEOSITE,private,real-ip", "GEOSITE,connectivity-check,real-ip",
-            "RULE-SET,自用fake-ip,real-ip", "GEOSITE,googlefcm,real-ip",
+            "RULE-SET,自用fake-ip,real-ip", "RULE-SET,自用直连规则,real-ip", "GEOSITE,googlefcm,real-ip",
             "GEOSITE,cn,real-ip", "GEOSITE,geolocation-cn,real-ip",
             "GEOSITE,gfw,fake-ip", "GEOSITE,geolocation-!cn,fake-ip",
             "MATCH,fake-ip"
