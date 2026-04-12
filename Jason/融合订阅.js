72a94de1fdacc020b919a214ec5f4b3f3b5ab113
TYDMX <69235273+TYDMX@users.noreply.github.com>
2026-04-12 21:12:06 +0800

    Update 融合订阅.js

--- Jason/融合订阅.js
+++ Jason/融合订阅.js
@@ -116,9 +116,9 @@ function main(config) {
         "udp-timeout": 300//秒
     };
     // --- 【DNS配置模板】 ---#h3=true
-    const 谷歌IP = ["8.8.8.8"]; const 谷歌DOT = ["tls://dns.google"]; const 谷歌DOH = ["https://dns.google/dns-query#🖥️ 代理服务"];
-    const cloudflare_IP = ["1.1.1.1"]; const cloudflare_DOT = ["tls://cloudflare-dns.com"]; const cloudflare_DOH = ["https://cloudflare-dns.com/dns-query#🖥️ 代理服务"];
-    const 阿里IP = ["223.5.5.5"]; const 阿里DOT = ["tls://dns.alidns.com"]; const 阿里DOH = ["https://dns.alidns.com/dns-query#h3=true"]; const 阿里QUIC = ["quic://dns.alidns.com#🇨🇳 直连"];
+    const 谷歌IP = ["8.8.8.8"]; const 谷歌DOT = ["tls://dns.google"]; const 谷歌DOH = ["https://dns.google/dns-query#🖥️ 代理服务&h3=true"];
+    const cloudflare_IP = ["1.1.1.1"]; const cloudflare_DOT = ["tls://cloudflare-dns.com"]; const cloudflare_DOH = ["https://cloudflare-dns.com/dns-query#🖥️ 代理服务&h3=true"];
+    const 阿里IP = ["223.5.5.5"]; const 阿里DOT = ["tls://dns.alidns.com"]; const 阿里DOH = ["https://dns.alidns.com/dns-query#h3=true"]; const 阿里QUIC = ["quic://dns.alidns.com"];
     const 腾讯IP = ["119.29.29.29"]; const 腾讯DOT = ["tls://dot.pub"]; const 腾讯DOH = ["https://doh.pub/dns-query","https://sm2.doh.pub/dns-query"];
     const 国外DNS = [
         ...谷歌DOH, 
