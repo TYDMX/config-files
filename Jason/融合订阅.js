7c063c9e31805315eb5342e9b6ef08e2a5c46adc
TYDMX <69235273+TYDMX@users.noreply.github.com>
2026-04-12 20:58:45 +0800

    Update 融合订阅.js

--- Jason/融合订阅.js
+++ Jason/融合订阅.js
@@ -118,7 +118,7 @@ function main(config) {
     // --- 【DNS配置模板】 ---#h3=true
     const 谷歌IP = ["8.8.8.8"]; const 谷歌DOT = ["tls://dns.google"]; const 谷歌DOH = ["https://dns.google/dns-query#🖥️ 代理服务"];
     const cloudflare_IP = ["1.1.1.1"]; const cloudflare_DOT = ["tls://cloudflare-dns.com"]; const cloudflare_DOH = ["https://cloudflare-dns.com/dns-query#🖥️ 代理服务"];
-    const 阿里IP = ["223.5.5.5"]; const 阿里DOT = ["tls://dns.alidns.com"]; const 阿里DOH = ["https://dns.alidns.com/dns-query#🇨🇳 直连&h3"]; const 阿里QUIC = ["quic://dns.alidns.com#🇨🇳 直连"];
+    const 阿里IP = ["223.5.5.5"]; const 阿里DOT = ["tls://dns.alidns.com"]; const 阿里DOH = ["https://dns.alidns.com/dns-query#🇨🇳 直连&h3=true"]; const 阿里QUIC = ["quic://dns.alidns.com#🇨🇳 直连"];
     const 腾讯IP = ["119.29.29.29"]; const 腾讯DOT = ["tls://dot.pub"]; const 腾讯DOH = ["https://doh.pub/dns-query#🇨🇳 直连"];
     const 国外DNS = [
         ...谷歌DOH, 
