0f53372de5bd90ff3892c82e6c8ee655cba941fe
TYDMX <69235273+TYDMX@users.noreply.github.com>
2026-04-14 14:10:18 +0800

    更新 融合订阅.js

--- Jason/融合订阅.js
+++ Jason/融合订阅.js
@@ -281,7 +281,7 @@ function main(config) {
         "OR,((GEOSITE,cn),(GEOSITE,geolocation-cn),(GEOIP,cn)),⬆️ 直连域名",
         //代理规则
         "GEOSITE,category-games-!cn,🎮 game",
-        //"AND,((NETWORK,UDP),(DST-PORT,443)),REJECT",
+        "AND,((NETWORK,UDP),(DST-PORT,443)),REJECT",
         "OR,((GEOSITE,openai),(GEOSITE,google-gemini),(GEOSITE,category-ai-!cn)),🤖 人工智能",
         "GEOSITE,spotify,🎵 音乐服务",
         "GEOSITE,paypal,💶 PayPal",
