5a88f1360d64e633d53d06e3d5c916c66af0313d
TYDMX <69235273+TYDMX@users.noreply.github.com>
2026-04-18 08:45:34 +0800

    更新 融合订阅.js

--- Jason/融合订阅.js
+++ Jason/融合订阅.js
@@ -118,7 +118,7 @@ function main(config) {
     // --- 【DNS配置模板】 ---
     const 谷歌IP = ["8.8.8.8"]; const 谷歌DOT = ["tls://dns.google"]; const 谷歌DOH = ["https://dns.google/dns-query#🇬 谷歌"];
     const cloudflare_IP = ["1.1.1.1"]; const cloudflare_DOT = ["tls://cloudflare-dns.com"]; const cloudflare_DOH = ["https://cloudflare-dns.com/dns-query#🖥️ 域名服务"];
-    const 阿里IP = ["223.5.5.5"]; const 阿里DOT = ["tls://dns.alidns.com"]; const 阿里DOH = ["https://dns.alidns.com/dns-query"]; const 阿里QUIC = ["quic://dns.alidns.com"];
+    const 阿里IP = ["223.5.5.5"]; const 阿里DOT = ["tls://dns.alidns.com"]; const 阿里DOH = ["https://dns.alidns.com/dns-query#🇨🇳 直连"]; const 阿里QUIC = ["quic://dns.alidns.com"];
     const 腾讯IP = ["119.29.29.29"]; const 腾讯DOT = ["tls://dot.pub"]; const 腾讯DOH = ["https://doh.pub/dns-query","https://sm2.doh.pub/dns-query"];
     const 国外DNS = [
         //...谷歌DOH, 
