a9c13b9e9c4907278da7a06a9bed88d85030f108
TYDMX <69235273+TYDMX@users.noreply.github.com>
2026-06-21 22:16:58 +0800

    Comment out RULE-SET for cn and geolocation

--- Jason/融合订阅.js
+++ Jason/融合订阅.js
@@ -177,9 +177,9 @@ function main(config) {
             "RULE-SET,fakeip_filter,real-ip",
             "RULE-SET,googlefcm,real-ip",
             //"RULE-SET,gfw,fake-ip",
+            //"RULE-SET,cn,real-ip",
             //"RULE-SET,geolocation-cn,real-ip",
             //"RULE-SET,geolocation-!cn,fake-ip",
-            //"RULE-SET,cn,real-ip",
             "MATCH,fake-ip"
         ],
         "default-nameserver": 阿里DNS,
